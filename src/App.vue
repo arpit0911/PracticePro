ad599ca7e7d9e8887420d16ecd42564d86c7ccd5
arpit singh <singharpit0911@gmail.com>
2021-06-11 10:56:09 +0530

    My first Commit

---
+++ src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <h1>Ninja Reaction Timer</h1>
+  <button class="btn" @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <Results v-if="showScore" :score="score"/>
+
+</template>
+
+<script>
+import Block from "./components/Block.vue";
+import Results from "./components/Results.vue";
+
+export default {
+  name: "App",
+  components: { Block , Results},
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showScore: false,
+
+    }
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000;
+      this.isPlaying = true;
+      this.showScore=false
+    },
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.showScore= true
+      this.isPlaying= false
+    },
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #444;
+  margin-top: 60px;
+}
+.btn{
+  text-align:  center;
+  padding: 10px 20px ;
+  background-color: rgb(69, 170, 170);
+  border-radius: 4px;
+  border : 1px solid rgb(14, 42, 51);
+  cursor: pointer;
+  font-size: 16px;
+  letter-spacing: 1px;
+  text-decoration: none;
+  color: white;
+  
+}
+.btn:hover{
+  background-color: rgb(57, 143, 143) ;
+}
+.btn[disabled]{
+  opacity: 0.2;
+  cursor: not-allowed;
+}
+</style>
---
+++ src/components/Results.vue
@@ -0,0 +1,29 @@
+<template>
+  <p>Reaction Time {{score}}ms <br><br>
+  {{rank}}</p>
+
+</template>
+
+<script>
+export default {
+props:['score'],
+data(){
+    return{
+        rank: null
+    }
+},
+mounted(){
+    if(this.score <=150){
+        this.rank ="Ninja Fingers"
+    }else if(this.score <= 300){
+        this.rank = "Rapid Reflexes"
+    }else {
+        this.rank = "Snail Pace..."
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
